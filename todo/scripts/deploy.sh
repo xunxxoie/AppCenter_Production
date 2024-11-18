efedf733b991509bc7dc7f339c89010c4f05776f
xunxxoie <ahh0520@naver.com>
2024-11-19 01:03:41 +0900

    [cicd] update files

--- todo/scripts/deploy.sh
+++ todo/scripts/deploy.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
-
 echo "> 현재 USER NAME을 가져옵니다."
 CURRENT_USER=$(whoami)
 
-echo "> download 폴더에서 build된 .jar 파일을 가져옵니다."
-cp /home/$CURRENT_USER/download/todo/builds/libs/*.jar /home/$CURRENT_USER/deploy
+echo "> download 폴더의 jar 파일을 deploy 폴더로 복사합니다."
+cp /home/$CURRENT_USER/download/todo/*.jar /home/$CURRENT_USER/deploy/
 
 echo "> deploy 폴더에서 plain.jar 파일을 찾고, 파일이 있다면 삭제합니다."
 find /home/$CURRENT_USER/deploy -name "*plain*" -delete 2>/dev/null || true
@@ -25,7 +24,6 @@ fi
 
 echo "> 새 애플리케이션을 시작합니다."
 cd /home/$CURRENT_USER/deploy
-
 nohup java -jar \
     -Dspring.profiles.active=prod \
     $(ls -t *.jar | grep -v 'plain' | head -n1) \
